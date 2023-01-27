b85fdff8f875387728ec4368414963af2861cd1a
federicos1 <fedes7777@gmail.com>
2023-01-27 08:05:02 -0300

    minor fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,12 +9,10 @@ import image from "@astrojs/image";
 // https://astro.build/config
 import svelte from "@astrojs/svelte";
 
-// https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), image(), svelte()],
-  output: "server",
-  adapter: vercel()
+
 });
